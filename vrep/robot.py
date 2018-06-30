b8d920ad6580fe66ec1f43944c2ea8fd53d781aa
Kamnev Yuriy <yukamnev@gmail.com>
2018-06-30 21:16:48 +0300

    EE vector is now ee pose

--- vrep/robot.py
+++ vrep/robot.py
@@ -2,6 +2,8 @@ from ur10_tf_dh import TF
 import numpy as np
 import ros
 from ik import ik
+from ur10_controller import UR10JointPositionController
+from profiler import timeit
 
 ikdamped = ik.damped_least_squares
 tf = ros.tf.transformations
@@ -21,7 +23,12 @@ class UR10(object):
         self._tf = TF(self.r, self.alpha, self.d, self.offset)
         self._q = np.zeros((6,))
 
+        self._ee_pose = np.eye(4)
+        TF._prepareZ(self._ee_pose, np.pi, 0.0274)
+
         self._jacobian = np.zeros((6, 6))
+
+        self._controller = UR10JointPositionController()
 
     @property
     def q(self):
@@ -32,23 +39,25 @@ class UR10(object):
         for i in range(v.shape[0]):
             self._q[i] = v[i]
 
+        self._controller.set_angles(self._q)
+
     def J(self, q=None):
         if q is None:
             q = self._q
 
-        vec = [0, 0, 0]
+        vec = np.eye(4)
         ee = self._tf.get_pose(q, vec, self._ee_frame, self._base_frame)
         ee_pos, ee_rot = UR10.vec_rot_from_matrix(ee)
 
         self._jacobian.fill(0)
 
         for i in range(self._base_frame, self._ee_frame):
-            pose = self._tf.get_pose(q, vec, self._ee_frame, i)
+            pose = self._tf.get_pose(q, vec, i, self._base_frame)
 
             position, orientation = UR10.vec_rot_from_matrix(pose)
 
             radius_vector = np.subtract(ee_pos, position)
-            rot_axis = (orientation[:, 2])    # Z axis
+            rot_axis = np.dot(orientation, [0, 0, 1])    # Z axis
 
             jacobian_vec = np.cross(rot_axis, radius_vector)
 
@@ -62,25 +71,27 @@ class UR10(object):
         target_pose = [target_position, targer_quaternion]
 
         res = ikdamped(self.J, self._q, source_pose, target_pose,
-                       self.get_pose, alpha=0.1, eps=0.1, damping_ratio=0.3,
+                       self.get_pose, alpha=0.1, eps=0.001, damping_ratio=0.3,
                        kp=1.0, ko=1.0, max_iter=5000)
 
         if res[0]:
-            return res[1]
+            return res[1], res[2]
 
-        return None
+        return None, None
 
     def get_pose(self, q=None):
-        vec = [0, 0, 0]
+        ee_pose = np.eye(4)
         if q is None:
             q = self.q
 
-        pose_relative_to_base = self._tf.get_pose(q, vec,
+        pose_relative_to_base = self._tf.get_pose(q, ee_pose,
                                                   self._ee_frame,
                                                   self._base_frame)
 
         world_pose = np.dot(self._base_frame_pose, pose_relative_to_base)
-        return UR10.vec_quat_from_matrix(world_pose)
+        ee_world_pose = np.dot(world_pose, self._ee_pose)
+
+        return UR10.vec_quat_from_matrix(ee_world_pose)
 
     @staticmethod
     def vec_quat_from_matrix(mat):
@@ -91,52 +102,3 @@ class UR10(object):
     def vec_rot_from_matrix(mat):
         return (mat[:, 3])[:-1], mat[:-1, :-1]
 
-
-def test_ur10():
-    ur10_world_position = np.asarray([-0.25, 0.25, 0.0447, 1])
-    base_frame_pose = np.eye(4)
-    base_frame_pose[:, 3] = ur10_world_position[:]
-
-    ur10 = UR10(base_frame_pose=base_frame_pose)
-
-    # ####################### FK #######################
-
-    # First position
-    q = np.zeros(6)
-    ur10.q = q
-    position, orientation = ur10.get_pose()
-    print(position)
-    print(np.asarray(tf.euler_from_quaternion(orientation, axes="szyx")) *
-          180./np.pi)
-
-    # Second position
-    q = np.asarray([np.pi/4., np.pi/2., -np.pi/4., -np.pi/2., -np.pi/6., np.pi])
-    ur10.q = q
-    position, orientation = ur10.get_pose()
-    print(position)
-    print(np.asarray(tf.euler_from_quaternion(orientation, axes="szyx")) *
-          180./np.pi)
-
-    return 0
-    # ################## IK #######################
-    ik_required_position = position
-    ik_required_orientation = orientation
-    ik_required_pose = [ik_required_position, ik_required_orientation]
-
-    ur10.q = np.zeros(6)
-    ik_q = ur10.solve_ik(ik_required_position, ik_required_orientation)
-
-    if ik_q is None:
-        print("FAILED TO SOLVE IK")
-    else:
-        ik_pose = ur10.get_pose(q=ik_q)
-
-        print("IK SOLVED SUCCESSFULLY")
-        print("Source angles: " + str(np.zeros((6,))))
-        print("Required angles: " + str(q))
-        print("Source pose: " + str(ur10.get_pose()))
-        print("Required pose: " + str(ik_required_pose))
-        print("IK pose: " + str(ik_pose))
-
-
-test_ur10()
